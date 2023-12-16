9dc57ef5fc2c891d96ec886ecd14d5402293cd45
George-Sush <juri.sushkin@yandex.ru>
2023-12-16 19:50:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+import json
 from kivy.core.window import Window
 from kivy.core.image import Image
 from kivy.uix.button import Button
@@ -12,13 +13,28 @@ from kivy.uix.togglebutton import ToggleButton
 class Base(App):
     def __init__(self):
         super().__init__()
-        self.button = Button(text="123", size_hint=(0.2, 0.5), pos_hint={"x": 0.3, "y": 0.2})
+        self.button = Button(text="123", size_hint=(0.2, 0.5), pos_hint={"x": 0.3, "y": 0.2}, background_color="green")
         self.box = RelativeLayout()
         self.box.add_widget(self.button)
+        self.map_buttons = []
+        self.create_map_buttons({"107": [(0.1, 0.2), "green"], "100": [(0.1, 0.2), "red"]})
 
     def build(self):
         return self.box
 
+    def create_map_buttons(self, json_map:dict):
+        a = 0
+        for key in json_map.keys():
+            self.map_buttons.append(Button(text=key, size_hint=json_map[key][0], pos_hint={"x": 0.1+(a*0.2), "y": 0.1+(a*0.2)}, background_color=json_map[key][1]))
+            a += 1
+        for buttons in self.map_buttons:
+            self.box.add_widget(buttons)
 
-Base().run()
-print(123)
+
+
+
+
+
+
+if __name__ == "__main__":
+    Base().run()
