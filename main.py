817a6d26e91425763df64d1817d48f233a7aed95
George-Sush <juri.sushkin@yandex.ru>
2023-12-16 14:41:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-import kivy
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.core.image import Image
+from kivy.uix.button import Button
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.properties import StringProperty
+from kivy.uix.togglebutton import ToggleButton
+
+
+class Base(App):
+    def __init__(self):
+        super().__init__()
+        self.button = Button(text="123", size_hint=(0.2, 0.5), pos_hint={"x": 0.3, "y": 0.2})
+        self.box = RelativeLayout()
+        self.box.add_widget(self.button)
+
+    def build(self):
+        return self.box
+
+
+Base().run()
+print(123)
